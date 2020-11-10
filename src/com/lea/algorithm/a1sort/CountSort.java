c7ee63a364fbd733a225b02512a3d0ba3db42893
liam <380639608@qq.com>
2020-11-10 20:23:19 +0800

    复习计数排序的优化

--- src/com/lea/algorithm/a1sort/CountSort.java
+++ src/com/lea/algorithm/a1sort/CountSort.java
@@ -40,9 +40,53 @@ public class CountSort {
         return sortArray;
     }
 
+
+    /*
+    优化都的 计数排序是稳定性的 时间复杂度是O(n+m)
+    在下列情况不适合用计数排序进行排序
+    1.当数列最大和最小值差距过大时，并不适合计数排序
+    2.当数列元素不是整数时，也不适合用计数排序
+     */
+    // 优化计数排序
+    public static int[] countSort2(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        // 1. 获取数组最大 最小的数，控制计数数组的大小
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        int d = max - min;
+        // 计数数组，扩大一位，装最后一个数. 统计对应元素个数
+        int[] countArray = new int[d + 1];
+        for (int i = 0; i < arr.length; i++) {
+            int index = arr[i] - min;
+            countArray[index]++;
+        }
+        // 计数数组变形. 从下标为1的索引开始，等于前面的元素值的和
+        for (int i = 1; i < countArray.length; i++) {
+            countArray[i] += countArray[i - 1];
+        }
+        // 倒序遍历原始数列，从统计数组找到正确的位置，输出结果数组
+        int[] sortedArray = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int index = arr[i] - min; // 得到在计数数组中的下标
+            int countArrayValue = countArray[index];
+            sortedArray[countArrayValue - 1] = arr[i];
+            countArray[index]--;
+        }
+        return sortedArray;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,3,10,5,6,3,9,8,7,6};
-        int[] countSort = countSort(arr);
+//        int[] countSort = countSort(arr);
+        arr = new int[]{99, 92, 94, 97};
+        int[] countSort = countSort2(arr);
         System.out.println(Arrays.toString(countSort));
     }
 }
